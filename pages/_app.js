5d8b6e0d75069b421e43d18c3eae529f1b1c8d08
Nestot Kenneth Aniar <ken.aniar.dev@gmail.com>
2022-03-11 03:02:12 +0800

    added gtag

--- pages/_app.js
+++ pages/_app.js
@@ -15,6 +15,10 @@ function MyApp({ Component, pageProps }) {
       <Script id='s' strategy='lazyOnload'>
         {`window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'G-RZHCE2NGS5');`}
       </Script>
+      <Script id='s' strategy='lazyOnload'>
+        {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-K2LK3W9')`}
+      </Script>
+
       <Component {...pageProps} />
     </>
   );
