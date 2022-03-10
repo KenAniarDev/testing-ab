1f92cf7405ea12a3f462b125d4f980f5b4d81966
Nestot Kenneth Aniar <ken.aniar.dev@gmail.com>
2022-03-10 19:25:49 +0800

    added GA

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script
+        strategy='lazyOnload'
+        src='https://www.googleoptimize.com/optimize.js?id=OPT-W4T2RGK'
+      />
+      <Script id='s' strategy='lazyOnload'>
+        {`window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'G-RZHCE2NGS5');`}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
