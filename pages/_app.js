995cc4128932533a73d46a11d37811d9b6198356
Nestot Kenneth Aniar <ken.aniar.dev@gmail.com>
2022-03-14 12:28:13 +0800

    add optimize

--- pages/_app.js
+++ pages/_app.js
@@ -12,6 +12,10 @@ function MyApp({ Component, pageProps }) {
       <Script id='s' strategy='lazyOnload'>
         {`window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'G-FMMGP3DJE7');`}
       </Script>
+      <Script
+        strategy='lazyOnload'
+        src='https://www.googleoptimize.com/optimize.js?id=OPT-PDXN32M'
+      ></Script>
       <Component {...pageProps} />
     </>
   );
