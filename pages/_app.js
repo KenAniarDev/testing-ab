1e1e28021418181e5884d6bb23e55471cb6b711e
Nestot Kenneth Aniar <ken.aniar.dev@gmail.com>
2022-03-14 12:18:16 +0800

    sad

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,17 @@
 import '../styles/globals.css';
 import Script from 'next/script';
-import TagManager from 'react-gtm-module';
-import { useEffect } from 'react';
 
 function MyApp({ Component, pageProps }) {
-  const tagManagerArgs = {
-    gtmId: 'GTM-NTJ2DR8',
-  };
-
-  useEffect(() => {
-    TagManager.initialize(tagManagerArgs);
-  }, []);
   return (
     <>
+      <Script
+        strategy='lazyOnload'
+        src='https://www.googletagmanager.com/gtag/js?id=G-FMMGP3DJE7'
+      />
+
+      <Script id='s' strategy='lazyOnload'>
+        {`window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'G-FMMGP3DJE7');`}
+      </Script>
       <Component {...pageProps} />
     </>
   );
